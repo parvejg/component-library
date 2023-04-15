dc90216ab38bdce5190a38f872780b31e454aaf9
parvejg <parvezpg321@gmail.com>
2023-04-15 13:57:56 +0530

    CSS UPDATE

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import './button.css'
+ export const Button =()=>{
+return <div>
+     <button className='clickBtn'>click</button>
+     <iframe
+  src="https://carbon.now.sh/embed?bg=rgba%28155%2C155%2C155%2C1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%2520%253Cbutton%2520className%253D%27clickBtn%27%253Eclick%253C%252Fbutton%253E"
+  style={{width: "521px", height: "205px", border:"0", transform: "scale(1)", overflow:"hidden"}}
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+<button  style={{color: "red"}}className='likeBtn btn'>like</button>
+<button className='subscribeBtn btn'>subscribe</button>
+<button className='startBtn btn'>start</button>
+</div>
+
+}
